14465cf3227e20f4e162ee8cd559683dfe03de2a
Trent <trentvarnes@icloud.com>
2022-12-13 09:23:15 -0500

    Updated DB connection

--- database/config/config.js
+++ database/config/config.js
@@ -1,13 +1,13 @@
 /**
- * Connects to my local database using my login credentials 
+ * Connects to my local database using my login credentials
  */
 
-import { Sequelize } from "sequelize";
+import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize('task', 'postgres', '1234', {
-    host: 'localhost',
-    dialect: 'postgres'
-  });
+const sequelize = new Sequelize('tasks', 'postgres', '1234', {
+  host: 'localhost',
+  dialect: 'postgres',
+});
 
 sequelize.sync();
 module.exports = sequelize;
